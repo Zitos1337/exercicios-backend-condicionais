6ced70c65b532a990b0b327c3d1acbe09db0ef56
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-22 16:11:38 -0300

    Update index.js

--- exercicios-backend-condicionais-master/07/index.js
+++ exercicios-backend-condicionais-master/07/index.js
@@ -1,11 +1,10 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; // em centavos
-
+const totalDeRendimentos = 3000000;
 if (portadoraDeDoenca || aposentada) {
-    console.log("ISENTA");
+    console.log("ISENTA")
 } else if (totalDeRendimentos <= 2855970) {
-    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE")
 } else {
-    console.log("PEGA LEAO");
+    console.log("PEGA LEAO")
 }
