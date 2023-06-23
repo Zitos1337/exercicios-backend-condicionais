c938ca12239371a4e04102198f255a34dec37728
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-23 14:58:31 -0300

    Update index.js

--- exercicios-backend-condicionais-master/14/index.js
+++ exercicios-backend-condicionais-master/14/index.js
@@ -1,12 +1,9 @@
-const quantidadeDeAguaIngerida = 2;
-let mensagem;
+const quantidadeDeAguaIngerida = 3.1;
 
 if (quantidadeDeAguaIngerida < 1.5) {
-    mensagem = "Risco Alto - Você está ingerindo pouquíssima água, beba mais água!";
+    console.log("Risco Alto - Você está ingerindo pouquissima água, beba mais água!")
 } else if (quantidadeDeAguaIngerida >= 1.5 && quantidadeDeAguaIngerida <= 3) {
-    mensagem = "Risco Moderado - Você está ingerindo pouca água, beba mais!";
+    console.log("Risco Moderado - Você está ingerindo pouca água, beba mais!")
 } else {
-    mensagem = "Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!";
+    console.log("Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!")
 }
-
-console.log(mensagem);
